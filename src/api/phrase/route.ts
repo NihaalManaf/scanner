4adc7b6cbcbed6d87a420b641f04f4a142fbfe8f
Frederick Emerson <frederick82004@gmail.com>
2024-06-14 20:55:35 +0800

    auth fix

--- src/api/phrase/route.ts
+++ src/api/phrase/route.ts
@@ -4,7 +4,7 @@ import { env } from "~/env";
 export async function GET(req: NextRequest) {
   const apiKey = req.headers.get('x-api-key');
   const serverKey = env.SERVER_API_KEY;
-  const authPhrase = env.SECRET_PHRASE;
+  const authPhrase = serverKey;
 
   if (!apiKey || (apiKey+"7d7c8e5f7e22") !== serverKey) {
     return new NextResponse(JSON.stringify({ message: 'Unauthorized' }), { status: 401, headers: { 'Content-Type': 'application/json' } });
