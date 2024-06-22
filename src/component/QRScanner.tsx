82bc5b2b21a5aeea1308b716c903ffcc566feaa8
Frederick Emerson <frederick82004@gmail.com>
2024-06-23 03:31:36 +0800

    more changes

--- src/component/QRScanner.tsx
+++ src/component/QRScanner.tsx
@@ -18,29 +18,6 @@ const QRScanner = () => {
   const [response, setResponse] = useState<responseType | null>(null);
  const[ifNextPressed, setPressed] = useState<boolean>(false);
  const [isProcessing, setProcessing] = useState<boolean>(false);
- const [scanner, setScanner] = useState<Html5QrcodeScanner>(new Html5QrcodeScanner(
-  'reader',
-  {
-    qrbox: {
-      width: 250,
-      height: 250,
-    },
-    fps: 10,
-    disableFlip: false
-  },
-  false
-));
-
- const getMessage = () => {
-  if (response?.status !== 'valid') {
-    return 'Invalid QR';
-  } else if (response?.status === 'valid' && !response?.enterStatus) {
-    return 'Already Entered';
-  } else {
-    return 'Good to Enter';
-  }
-};
-
     const handleAuth = async (code:string) =>{
     
         const data = {
@@ -60,47 +37,46 @@ const QRScanner = () => {
     setResponse(result)
   }
   
-  const processSuccess = async (result: string) => { 
-    scanner.pause()
-    await handleAuth(result)
-    setProcessing(false)
-};
 
-const success = (result: string) => {
-  if (!isProcessing && !showResult) {
-    setProcessing(true)
-    //eslint-disable-next-line @typescript-eslint/no-floating-promises
-    processSuccess(result);
-  }
-};
-
-
-const error = (err: string) => {
-  console.warn(err);
-};
 
   const handleConfirm = () => {
     setShowResult(false)
-    scanner.resume()
   }
  
     useEffect(() => {
-    
-      const n_scanner = new Html5QrcodeScanner(
-        'reader',
-        {
-          qrbox: {
-            width: 250,
-            height: 250,
+        const scanner = new Html5QrcodeScanner(
+          'reader',
+          {
+            qrbox: {
+              width: 250,
+              height: 250,
+            },
+            fps: 10,
+            disableFlip: false
           },
-          fps: 10,
-          disableFlip: false
-        },
-        false
-      );
-      
-      setScanner(n_scanner)
+          false
+        );
+    
+        const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
+        const processSuccess = async (result: string) => {               
+            await handleAuth(result)
+             while(showResult){
+              await sleep(100)
+             }
+        }
+
+        const success = (result: string) => {
+          if (!isProcessing && !showResult) {
+            scanner.pause
+            processSuccess(result);
+          }
+        };
+    
+        const error = (err: string) => {
+          console.warn(err);
+        };
+    
         scanner.render(success, error);
     
         return () => {
@@ -108,33 +84,13 @@ const error = (err: string) => {
             console.error("Failed to clear ", error);
           });
         };
-      }, [scanner,success]);
+      }, []);
 
       return(
         <>
-        <div  id="reader" className="w-96 flex flex-col justify-center items-center"></div>
-
+        <div className="w-96 flex flex-col justify-center items-center" id="reader"></div>
         {showResult && 
-              (<div
-                className={`fixed top-0 left-0 w-full h-full flex justify-center items-center ${
-                  response?.status === 'valid' && response?.enterStatus ? 'bg-green-500' : 'bg-red-500'
-                } bg-opacity-50`}
-              >
-                <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-                  <div className="flex flex-col items-start mb-2">
-                  <h1 className='mt-2 font-bold text-2xl'> 🎫 Ticket-ID # {response?.ticket_id}</h1>
-                  <p className='text-gray-500'>Booking ID : {response?.bookingId}</p>
-                  <p className='mt-4'> <b>Name</b> : {response?.name} # <b>{response?.ticket_number} </b> </p>
-                  </div>
-                  <p className="font-bold text-2xl text-center m-8"> {getMessage()}</p>
-                  <button
-                        className="bg-black text-white px-4 py-2 rounded-lg shadow-lg transform transition-transform duration-200 hover:shadow-xl active:scale-95 mr-2"
-                        onClick={handleConfirm}
-                      >
-                         Next
-                      </button>
-                </div>
-              </div>)
+              (<TicketModal response={response} showResult={showResult} setShowResult={setShowResult} setPressed={setPressed}/>)
 }
     
       
