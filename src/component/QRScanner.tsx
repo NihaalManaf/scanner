747db0d58e50d0e27b77867336dd758c3b687b84
Frederick Emerson <frederick82004@gmail.com>
2024-06-23 02:49:03 +0800

    change

--- src/component/QRScanner.tsx
+++ src/component/QRScanner.tsx
@@ -18,6 +18,17 @@ const QRScanner = () => {
   const [response, setResponse] = useState<responseType | null>(null);
  const[ifNextPressed, setPressed] = useState<boolean>(false);
  const [isProcessing, setProcessing] = useState<boolean>(false);
+
+ const getMessage = () => {
+  if (response?.status !== 'valid') {
+    return 'Invalid QR';
+  } else if (response?.status === 'valid' && !response?.enterStatus) {
+    return 'Already Entered';
+  } else {
+    return 'Good to Enter';
+  }
+};
+
     const handleAuth = async (code:string) =>{
     
         const data = {
@@ -38,40 +49,42 @@ const QRScanner = () => {
   }
   
   const processSuccess = async (result: string) => { 
+    scanner.pause()
     await handleAuth(result)
     setProcessing(false)
 };
 
+const success = (result: string) => {
+  if (!isProcessing && !showResult) {
+    setProcessing(true)
+    //eslint-disable-next-line @typescript-eslint/no-floating-promises
+    processSuccess(result);
+  }
+};
+
+const scanner = new Html5QrcodeScanner(
+  'reader',
+  {
+    qrbox: {
+      width: 250,
+      height: 250,
+    },
+    fps: 10,
+    disableFlip: false
+  },
+  false
+);
+
+const error = (err: string) => {
+  console.warn(err);
+};
+
   const handleConfirm = () => {
     setShowResult(false)
+    scanner.resume()
   }
  
     useEffect(() => {
-        const scanner = new Html5QrcodeScanner(
-          'reader',
-          {
-            qrbox: {
-              width: 250,
-              height: 250,
-            },
-            fps: 10,
-            disableFlip: false
-          },
-          false
-        );
-    
-
-        const success = (result: string) => {
-          if (!isProcessing && !showResult) {
-            setProcessing(true)
-            //eslint-disable-next-line @typescript-eslint/no-floating-promises
-            processSuccess(result);
-          }
-        };
-    
-        const error = (err: string) => {
-          console.warn(err);
-        };
     
         scanner.render(success, error);
     
@@ -86,7 +99,26 @@ const QRScanner = () => {
         <>
         <div className="w-96 flex flex-col justify-center items-center" id="reader"></div>
         {showResult && 
-              (<TicketModal response={response} showResult={showResult} setShowResult={setShowResult} setPressed={setPressed}/>)
+              (<div
+                className={`fixed top-0 left-0 w-full h-full flex justify-center items-center ${
+                  response?.status === 'valid' && response?.enterStatus ? 'bg-green-500' : 'bg-red-500'
+                } bg-opacity-50`}
+              >
+                <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+                  <div className="flex flex-col items-start mb-2">
+                  <h1 className='mt-2 font-bold text-2xl'> 🎫 Ticket-ID # {response?.ticket_id}</h1>
+                  <p className='text-gray-500'>Booking ID : {response?.bookingId}</p>
+                  <p className='mt-4'> <b>Name</b> : {response?.name} # <b>{response?.ticket_number} </b> </p>
+                  </div>
+                  <p className="font-bold text-2xl text-center m-8"> {getMessage()}</p>
+                  <button
+                        className="bg-black text-white px-4 py-2 rounded-lg shadow-lg transform transition-transform duration-200 hover:shadow-xl active:scale-95 mr-2"
+                        onClick={handleConfirm}
+                      >
+                         Next
+                      </button>
+                </div>
+              </div>)
 }
     
       
