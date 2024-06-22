dd9dd9ca0844d84497135a8842319d63fa07ab1a
Frederick Emerson <frederick82004@gmail.com>
2024-06-23 05:56:26 +0800

    nvm im not that bad

--- src/component/QRScanner.tsx
+++ src/component/QRScanner.tsx
@@ -65,7 +65,9 @@ const QRScanner = () => {
 
         const success = (result: string) => {
             // eslint-disable-next-line  @typescript-eslint/no-floating-promises
+            if(!showResult){
             processSuccess(result)
+            }
         };
     
         const error = (err: string) => {
@@ -80,7 +82,7 @@ const QRScanner = () => {
             console.error("Failed to clear ", error);
           });
         };
-      }, []);
+      }, [showResult]);
 
       return(
         <>
