476e6f2ef7cae5e339794f9f252449db5cbd02bd
Frederick Emerson <frederick82004@gmail.com>
2024-06-23 06:16:39 +0800

    ehh im sick of htis

--- src/component/QRScanner.tsx
+++ src/component/QRScanner.tsx
@@ -42,12 +42,6 @@ const QRScanner = () => {
    }
   }
   
-
-
-  const handleConfirm = () => {
-    setShowResult(false)
-  }
- 
     useEffect(() => {
         const scanner = new Html5QrcodeScanner(
           'reader',
@@ -92,7 +86,26 @@ const QRScanner = () => {
         <>
         <div className="w-96 flex flex-col justify-center items-center" id="reader"></div>
         {showResult && 
-              (<TicketModal response={response} showResult={showResult} setShowResult={setShowResult} />)
+              (<div
+                className={`fixed top-0 left-0 w-full h-full flex justify-center items-center ${
+                  response?.status === 'valid' && response?.enterStatus ? 'bg-green-500' : 'bg-red-500'
+                } bg-opacity-50`}
+              >
+                <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+                  <div className="flex flex-col items-start mb-2">
+                  <h1 className='mt-2 font-bold text-2xl'> 🎫 Ticket-ID # {response?.ticket_id}</h1>
+                  <p className='text-gray-500'>Booking ID : {response?.bookingId}</p>
+                  <p className='mt-4'> <b>Name</b> : {response?.name} # <b>{response?.ticket_number} </b> </p>
+                  </div>
+                  <p className="font-bold text-2xl text-center m-8"> {getMessage()}</p>
+                  <button
+                        className="bg-black text-white px-4 py-2 rounded-lg shadow-lg transform transition-transform duration-200 hover:shadow-xl active:scale-95 mr-2"
+                        onClick={handleConfirm}
+                      >
+                         Next
+                      </button>
+                </div>
+              </div>)
 }
     
       
--- src/component/TicketModal.tsx
+++ src/component/TicketModal.tsx
@@ -34,26 +34,7 @@ const TicketModal: React.FC<TicketComponentProps> = ({ response, showResult, set
   }
 
   return showResult && (
-    <div
-    className={`fixed top-0 left-0 w-full h-full flex justify-center items-center ${
-      response?.status === 'valid' && response?.enterStatus ? 'bg-green-500' : 'bg-red-500'
-    } bg-opacity-50`}
-  >
-    <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-      <div className="flex flex-col items-start mb-2">
-      <h1 className='mt-2 font-bold text-2xl'> 🎫 Ticket-ID # {response?.ticket_id}</h1>
-      <p className='text-gray-500'>Booking ID : {response?.bookingId}</p>
-      <p className='mt-4'> <b>Name</b> : {response?.name} # <b>{response?.ticket_number} </b> </p>
-      </div>
-      <p className="font-bold text-2xl text-center m-8"> {getMessage()}</p>
-      <button
-            className="bg-black text-white px-4 py-2 rounded-lg shadow-lg transform transition-transform duration-200 hover:shadow-xl active:scale-95 mr-2"
-            onClick={handleConfirm}
-          >
-             Next
-          </button>
-    </div>
-  </div>
+    <></>
   );
 };
 
