458d190e4a4f6edf1e6bc95ff149f6c35701415a
Frederick Emerson <frederick82004@gmail.com>
2024-10-16 11:31:22 +0800

    logs added

--- src/app/api/check/route.ts
+++ src/app/api/check/route.ts
@@ -6,18 +6,24 @@ interface ResponseType {
   name: string;
   enterStatus: boolean;
   ticket_id: string;
-  bookingId: string,
+  bookingId: string;
   ticket_number: string;
 }
 
 export async function POST(req: NextRequest) {
+  console.log('Received a request at:', new Date().toISOString());
+
+  const startTime = Date.now(); // Start time for measuring response time
+
   try {
-    const {code } = await req.json() as { code: string };
+    const { code } = await req.json() as { code: string };
 
     const data = {
       auth: env.SECRET_API_KEY,
       qr: code
     };
+
+    console.log('Sending request to external API with code:', code);
 
     const response = await fetch("https://icsnavaratri-65310ca8dfd6.herokuapp.com/scan", {
       method: 'POST',
@@ -27,17 +33,26 @@ export async function POST(req: NextRequest) {
       body: JSON.stringify(data)
     });
 
+    const endTime = Date.now(); // End time after fetching the response
+    console.log('Received response from external API in:', endTime - startTime, 'ms');
+
+    // Check if the response is OK
     if (!response.ok) {
-      return new NextResponse(JSON.stringify({ message: 'Failed to fetch data from external API' }), { status: response.status });
+      const errorMessage = await response.text();
+      console.error('Error from external API:', errorMessage || 'Failed to fetch data from external API');
+      return new NextResponse(JSON.stringify({ message: errorMessage || 'Failed to fetch data from external API' }), { status: response.status });
     }
 
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    // Ensure we correctly cast the response
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const result: ResponseType = await response.json();
-   // console.log(response)
 
+    console.log('Sending back response at:', new Date().toISOString(), 'with data:', result);
+    
     return new NextResponse(JSON.stringify(result), { status: 200, headers: { 'Content-Type': 'application/json' } });
   } catch (error) {
     console.error('Error:', error);
     return new NextResponse(JSON.stringify({ message: 'An error occurred while processing your request' }), { status: 500, headers: { 'Content-Type': 'application/json' } });
   }
 }
+
