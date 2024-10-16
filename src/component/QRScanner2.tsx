a4474f2c2e3f690a48298be29a23e15d26b45622
Frederick Emerson <frederick82004@gmail.com>
2024-10-16 10:31:38 +0800

    more fixes

--- src/component/QRScanner2.tsx
+++ src/component/QRScanner2.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect, useRef } from "react";
 import QrScanner from "qr-scanner";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "~/components/ui/card"
 
 interface responseType {
   status: string;
